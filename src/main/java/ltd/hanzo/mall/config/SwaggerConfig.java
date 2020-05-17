9d327c5aaaec76241394cc754a8a0386fc93f32b
haoyu <2469653218@qq.com>
2020-05-17 22:05:36 +0800

    Swagger-UI接口文档Config配置

---
+++ src/main/java/ltd/hanzo/mall/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package ltd.hanzo.mall.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author 皓宇QAQ
+ * @email 2469653218@qq.com
+ * @Date 2020/5/17 20:36
+ * @link https://github.com/Tianhaoy/hanzomall
+ * @Description: Swagger2API文档的配置
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为当前包下controller生成API文档
+                .apis(RequestHandlerSelectors.basePackage("ltd.hanzo.mall.controller"))
+                //为有@Api注解的Controller生成API文档
+//                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                //为有@ApiOperation注解的方法生成API文档
+//                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("半藏商城")
+                .description("半藏商城-SwaggerUI-API-接口文档")
+                .version("1.0")
+                .build();
+    }
+}
