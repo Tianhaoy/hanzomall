eb0c3faa3e5dc3e86e4c6fb900ae3f7f7d389b5e
haoyu <2469653218@qq.com>
2020-05-17 22:24:12 +0800

    swagger-UI-响应结果对象

--- src/main/java/ltd/hanzo/mall/util/Result.java
+++ src/main/java/ltd/hanzo/mall/util/Result.java
@@ -1,5 +1,8 @@
 package ltd.hanzo.mall.util;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.io.Serializable;
 
 /**
@@ -9,10 +12,14 @@ import java.io.Serializable;
  * @link https://github.com/Tianhaoy/hanzomall
  * 实现了Serializable接口 序列化
  */
+@ApiModel(description = "响应对象")
 public class Result<T> implements Serializable {
     private static final long serialVersionUID = 1L;
+    @ApiModelProperty(value = "响应码", name = "resultCode", required = true,example="200")
     private int resultCode;
+    @ApiModelProperty(value = "响应消息", name = "message", required = true,example="SUCCESS")
     private String message;
+    @ApiModelProperty(value = "响应数据", name = "data",example="json串")
     private T data;
 
     public Result() {
